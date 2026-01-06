d70598ba0f9cb3aff99eddb1ee72bd5c5a542e3b
anshu <anshu.ac.dv@gmail.com>
2026-01-06 17:19:19 +0530

    Initial Commit

--- Array.py
+++ Array.py
@@ -26,3 +26,28 @@ a.reverse()
 print("Reversed array elements are:")
 for i in range(0,len(a)):
     print(a[i], end=' ')
+
+
+print("\n")
+a.insert(2, 10)
+print("Array elements after insertion are:")
+for i in range(0,len(a)):
+    print(a[i], end=' ')
+
+print("\n")
+a.append(11)
+print("Array elements after appending are:")
+for i in range(0,len(a)):
+    print(a[i], end=' ')
+
+print("\n")
+a.remove(3)
+print("Array elements after removing are:")
+for i in range(0,len(a)):
+    print(a[i], end=' ')
+
+print("\n")
+copyarray = array(a.typecode, (x*2 for x in a))
+print("Copied array elements are:")
+for i in range(0,len(copyarray)):
+    print(copyarray[i], end=' ')
