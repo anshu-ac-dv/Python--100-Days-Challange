8abc7e1247639eb1a7f14a836b9db022c5f91f72
anshu <anshu.ac.dv@gmail.com>
2026-02-22 15:57:31 +0530

    initial commit

--- Arrays/arrayPractice.py
+++ Arrays/arrayPractice.py
@@ -1,22 +1,7 @@
-from array import*
+import array
 
+var = array.array('i', [1, 2, 3, 4, 5])
+print(var)  
 
-a = array('i', [1, 2, 3, 4, 5])
-print(a)
-print("\n")
-print("Array elements are:")
 for i in range(0,5):
-    print(a[i], end=' ')
-print("\n")
-print("Array elements using for loop:")
-for i in range(0,len(a)):
-    print(a[i], end=' ')
-print("\n")
-print(a.typecode)
-print("Array item size is:", a.itemsize)
-print("\n")
-a.reverse()
-print("Reversed array elements are:")
-for i in range(0,len(a)):
-    print(a[i], end=' ')
-print("\n")
+    print(var[i], end=' ')
