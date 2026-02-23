76551d9e8fa6e4026cd2681f048ec30f61127722
anshu <anshu.ac.dv@gmail.com>
2026-02-23 09:58:16 +0530

    Initial Commit

---
+++ Arrays/numpyArrange.py
@@ -0,0 +1,7 @@
+from numpy import*
+
+a = arange(10,20,2)
+
+print("Elements are:")
+for i in a:
+    print(i, end=',')
---
+++ Arrays/numpyLinspace.py
@@ -0,0 +1,6 @@
+from numpy import*
+
+a = linspace(0,20,5)
+print("Array elements are:")
+for i in a:
+    print(i, end=',')
