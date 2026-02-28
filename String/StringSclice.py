207e9eccdc651fa1bdba180b6fe0d63bdbdcf68a
anshu <anshu.ac.dv@gmail.com>
2026-02-28 11:29:23 +0530

    initial Commmit

--- String/StringSclice.py
+++ String/StringSclice.py
@@ -4,5 +4,11 @@ chai = "Masala Chai"
 print(chai)
 
 chai_sclice = chai[0:6]
+chai_sclice1 = chai[0:6:2]
+chai_sclice2 = chai[7:11]
+chai_sclice3 = chai[7:11:2]
 
 print(chai_sclice)
+print(chai_sclice1)
+print(chai_sclice2)
+print(chai_sclice3)
