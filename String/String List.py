8f9b399b2a6769d657d23f4a141950d26a579cae
anshu <anshu.ac.dv@gmail.com>
2026-02-28 11:21:04 +0530

    Initial Commit

---
+++ String/String List.py
@@ -0,0 +1,5 @@
+chai = "Masala Chai"
+print(chai)
+
+first_name = chai[0]
+print(first_name)
---
+++ String/StringSclice.py
@@ -0,0 +1,8 @@
+chai = "Lemon Chai"
+chai = "Masala Chai"
+
+print(chai)
+
+chai_sclice = chai[0:6]
+
+print(chai_sclice)
